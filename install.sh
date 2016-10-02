547e1202a83bc077274beb1b2a4d03f60676583c
Sinisa Kusic <contact.sinisa@gmail.com>
2016-10-02 10:59:39 +0200

    Update

--- install.sh
+++ install.sh
@@ -13,9 +13,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap caskroom/cask
 brew tap caskroom/fonts
 brew tap homebrew/versions
-brew tap caskroom/versions
 brew tap homebrew/dupes
-brew tap homebrew/homebrew-php
 brew install brew-cask
 
 # Install zsh
@@ -27,7 +25,7 @@ chsh -s /usr/local/bin/zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # Bunch of symlinks
-ln -sfv "$DOTFILES_DIR/.zprofile" ~
+ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/.gitmessage" ~
