a16d368cd46e5bea7ef6e7808f8e289c7915613c
Sinisa Kusic <contact.sinisa@gmail.com>
2023-02-24 09:34:01 +0100

    Fix the nvim-tree opening on VimEnter

--- config/nvim/lua/ku5ic/plugins/nvim-tree.lua
+++ config/nvim/lua/ku5ic/plugins/nvim-tree.lua
@@ -4,6 +4,37 @@ if not setup then
 	return
 end
 
+local function open_nvim_tree(data)
+	-- is real file
+	local is_real_file = vim.fn.filereadable(data.file) == 1
+
+	-- buffer is a [No Name]
+	local is_no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+	-- buffer is a directory
+	local is_directory = vim.fn.isdirectory(data.file) == 1
+
+	-- change to the directory
+	-- open the tree
+	if is_directory then
+		vim.cmd.cd(data.file)
+		require("nvim-tree.api").tree.open()
+	end
+
+	-- open the tree and find the file
+	if is_real_file then
+		require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+		return
+	end
+
+	-- do nothing
+	if is_no_name then
+		return
+	end
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 -- recommended settings from nvim-tree documentation
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
