2ea4d685b419d1aea0e72329e2e0a7ee986af79f
Sinisa Kusic <contact.sinisa@gmail.com>
2023-04-29 08:48:27 +0200

    Comment out nvim alpha

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -145,38 +145,38 @@ return {
 		},
 	},
 
-	{
-		"goolord/alpha-nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		enabled = false,
-		config = function()
-			local alpha = require("alpha")
-            local dashboard = require("alpha.themes.dashboard")
-            dashboard.section.header.val = {
-				[[       )                                                                    ]],
-				[[    ( /(             (                 (                                    ]],
-				[[    )\())     (      )\   (     (      )\    (  (     (           )     (   ]],
-				[[   ((_)\ (   ))\  ((((_)( )(   ))\  ((((_)(  )\))(   ))\(   (    (     ))\  ]],
-				[[  __ ((_))\ /((_)  )\ _ )(()\ /((_)  )\ _ )\((_)()\ /((_)\  )\   )\  '/((_) ]],
-				[[  \ \ / ((_|_))(   (_)_\(_|(_|_))    (_)_\(_)(()((_|_))((_)((_)_((_))(_))   ]],
-				[[   \ V / _ \ || |   / _ \| '_/ -_)    / _ \ \ V  V / -_|_-< _ \ '  \() -_)  ]],
-				[[    |_|\___/\_,_|  /_/ \_\_| \___|   /_/ \_\ \_/\_/\___/__|___/_|_|_|\___|  ]],
-				[[                                                                            ]],
-			}
-
-			dashboard.section.buttons.val = {
-				dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
-				dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
-			}
-			local handle = io.popen('fortune')
-			local fortune = handle:read("*a")
-			handle:close()
-			dashboard.section.footer.val = fortune
-			dashboard.config.opts.noautocmd = true
-			vim.cmd[[autocmd User AlphaReady echo 'ready']]
-			alpha.setup(dashboard.config)
-		end,
-	}
+	-- {
+	-- 	"goolord/alpha-nvim",
+	-- 	dependencies = {
+	-- 		"nvim-tree/nvim-web-devicons",
+	-- 	},
+	-- 	enabled = false,
+	-- 	config = function()
+	-- 		local alpha = require("alpha")
+ --            local dashboard = require("alpha.themes.dashboard")
+ --            dashboard.section.header.val = {
+	-- 			[[       )                                                                    ]],
+	-- 			[[    ( /(             (                 (                                    ]],
+	-- 			[[    )\())     (      )\   (     (      )\    (  (     (           )     (   ]],
+	-- 			[[   ((_)\ (   ))\  ((((_)( )(   ))\  ((((_)(  )\))(   ))\(   (    (     ))\  ]],
+	-- 			[[  __ ((_))\ /((_)  )\ _ )(()\ /((_)  )\ _ )\((_)()\ /((_)\  )\   )\  '/((_) ]],
+	-- 			[[  \ \ / ((_|_))(   (_)_\(_|(_|_))    (_)_\(_)(()((_|_))((_)((_)_((_))(_))   ]],
+	-- 			[[   \ V / _ \ || |   / _ \| '_/ -_)    / _ \ \ V  V / -_|_-< _ \ '  \() -_)  ]],
+	-- 			[[    |_|\___/\_,_|  /_/ \_\_| \___|   /_/ \_\ \_/\_/\___/__|___/_|_|_|\___|  ]],
+	-- 			[[                                                                            ]],
+	-- 		}
+	--
+	-- 		dashboard.section.buttons.val = {
+	-- 			dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+	-- 			dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
+	-- 		}
+	-- 		local handle = io.popen('fortune')
+	-- 		local fortune = handle:read("*a")
+	-- 		handle:close()
+	-- 		dashboard.section.footer.val = fortune
+	-- 		dashboard.config.opts.noautocmd = true
+	-- 		vim.cmd[[autocmd User AlphaReady echo 'ready']]
+	-- 		alpha.setup(dashboard.config)
+	-- 	end,
+	-- }
 }
