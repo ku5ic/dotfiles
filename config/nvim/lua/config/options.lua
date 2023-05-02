d400f651a31834baba0f2b8f06d89c50421b1c0f
Sinisa Kusic <contact.sinisa@gmail.com>
2023-05-02 10:13:05 +0200

    Add fzf to neovim's runtime path

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -66,3 +66,4 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+opt.runtimepath:append("/opt/homebrew/bin/fzf") -- add fzf to runtimepath
