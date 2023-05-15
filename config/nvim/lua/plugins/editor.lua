ff200c287ba054f372c504a9551f0c105d9dcd66
Sinisa Kusic <contact.sinisa@gmail.com>
2023-05-15 12:49:38 +0200

    Reformat editor.lua

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -3,8 +3,8 @@ return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		keys = {
-			{ "<C-enter>", "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
-			{ "<C-S-enter>", "<cmd>NeoTreeReveal<cr>", desc = "Reveal In NeoTree" },
+			{ "<C-enter>",   "<cmd>Neotree toggle<cr>", desc = "Toggle NeoTree" },
+			{ "<C-S-enter>", "<cmd>NeoTreeReveal<cr>",  desc = "Reveal In NeoTree" },
 		},
 		dependencies = {
 			"nvim-lua/plenary.nvim",
@@ -61,40 +61,41 @@ return {
 	},
 
 	-- fuzzy finder
-	{"nvim-telescope/telescope-fzf-native.nvim", build = "make"},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	{
-		"nvim-telescope/telescope.nvim", tag = '0.1.1',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		cmd = "Telescope",
 		-- version = false, -- telescope did only one release, so use HEAD for now
 		keys = {
-			{ "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
-			{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Find in Files (Grep)" },
-			{ "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+			{ "<leader>,",  "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
+			{ "<leader>/",  "<cmd>Telescope live_grep<cr>",                     desc = "Find in Files (Grep)" },
+			{ "<leader>:",  "<cmd>Telescope command_history<cr>",               desc = "Command History" },
 			-- find
-			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (root dir)" },
-			{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>",                       desc = "Buffers" },
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>",                    desc = "Find Files (root dir)" },
+			{ "<leader>fr", "<cmd>Telescope oldfiles<cr>",                      desc = "Recent" },
 			-- git
-			{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
-			{ "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
+			{ "<leader>gc", "<cmd>Telescope git_commits<CR>",                   desc = "commits" },
+			{ "<leader>gs", "<cmd>Telescope git_status<CR>",                    desc = "status" },
 			-- search
-			{ "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-			{ "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-			{ "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-			{ "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-			{ "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
-			{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-			{ "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-			{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-			{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-			{ "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-			{ "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-			{ "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+			{ "<leader>sa", "<cmd>Telescope autocommands<cr>",                  desc = "Auto Commands" },
+			{ "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>",     desc = "Buffer" },
+			{ "<leader>sc", "<cmd>Telescope command_history<cr>",               desc = "Command History" },
+			{ "<leader>sC", "<cmd>Telescope commands<cr>",                      desc = "Commands" },
+			{ "<leader>sd", "<cmd>Telescope diagnostics<cr>",                   desc = "Diagnostics" },
+			{ "<leader>sh", "<cmd>Telescope help_tags<cr>",                     desc = "Help Pages" },
+			{ "<leader>sH", "<cmd>Telescope highlights<cr>",                    desc = "Search Highlight Groups" },
+			{ "<leader>sk", "<cmd>Telescope keymaps<cr>",                       desc = "Key Maps" },
+			{ "<leader>sM", "<cmd>Telescope man_pages<cr>",                     desc = "Man Pages" },
+			{ "<leader>sm", "<cmd>Telescope marks<cr>",                         desc = "Jump to Mark" },
+			{ "<leader>so", "<cmd>Telescope vim_options<cr>",                   desc = "Options" },
+			{ "<leader>sR", "<cmd>Telescope resume<cr>",                        desc = "Resume" },
 		},
 		config = function()
-			require('telescope').load_extension('fzf')
-		end
+			require("telescope").load_extension("fzf")
+		end,
 	},
 
 	{
@@ -113,16 +114,16 @@ return {
 	-- todo comments
 	{
 		"folke/todo-comments.nvim",
-		cmd = {"TodoTelescope" },
+		cmd = { "TodoTelescope" },
 		event = { "BufReadPost", "BufNewFile" },
 		config = true,
 		-- stylua: ignore
 		keys = {
-			{ "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-			{ "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-			{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+			{ "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+			{ "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+			{ "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
 		},
 	},
 
-	{"tpope/vim-fugitive"},
+	{ "tpope/vim-fugitive" },
 }
