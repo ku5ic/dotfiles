58b258ac2f7e2859bc615f8c8e69a3657ffa0c34
Sinisa Kusic <contact.sinisa@gmail.com>
2024-01-15 12:24:38 +0100

    Hide scrollbars in WezTerm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -33,7 +33,7 @@ config.native_macos_fullscreen_mode = true
 config.window_background_opacity = 0.95
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
-config.enable_scroll_bar = true
+config.enable_scroll_bar = false
 config.enable_csi_u_key_encoding = true
 config.audible_bell = "Disabled"
 
