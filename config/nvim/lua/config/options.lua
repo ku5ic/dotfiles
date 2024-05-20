b8885066e0937a64e1d31fe24dfbdfeed720a58a
Sinisa Kusic <contact.sinisa@gmail.com>
2024-05-20 09:32:15 +0200

    refactor(nvim-config): remove redundant tab label configuration
    The `guitablabel` option in Neovim configuration was removed as it was redundant. The window bar configuration (`winbar`) already provides the necessary information about the file name and its modified status.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -11,7 +11,6 @@ opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation & folding
-opt.guitablabel = "%t" -- show file name in tab label
 opt.winbar = "%=%m %f" -- show file name and modified status in window bar
 opt.expandtab = true -- expand tab to spaces
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
