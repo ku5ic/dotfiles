7b1cb4a85d6ce910dc51d8726c169ad3ae080773
Sinisa Kusic <contact.sinisa@gmail.com>
2024-06-05 12:51:37 +0200

    feat(nvim): add precognition.nvim
    - Add precognition.nvim plugin to assist with discovering motions for buffer navigation

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -109,5 +109,34 @@ return {
 		},
 	},
 
+	-- vim-fugitive is a Git wrapper so awesome, it should be illegal
 	{ "tpope/vim-fugitive" },
+
+	-- precognition.nvim assists with discovering motions (Both vertical and horizontal) to navigate your current buffer
+	{
+		"tris203/precognition.nvim",
+		config = {
+			-- startVisible = true,
+			-- showBlankVirtLine = true,
+			-- highlightColor = { link = "Comment" },
+			-- hints = {
+			--      Caret = { text = "^", prio = 2 },
+			--      Dollar = { text = "$", prio = 1 },
+			--      MatchingPair = { text = "%", prio = 5 },
+			--      Zero = { text = "0", prio = 1 },
+			--      w = { text = "w", prio = 10 },
+			--      b = { text = "b", prio = 9 },
+			--      e = { text = "e", prio = 8 },
+			--      W = { text = "W", prio = 7 },
+			--      B = { text = "B", prio = 6 },
+			--      E = { text = "E", prio = 5 },
+			-- },
+			-- gutterHints = {
+			--     G = { text = "G", prio = 10 },
+			--     gg = { text = "gg", prio = 9 },
+			--     PrevParagraph = { text = "{", prio = 8 },
+			--     NextParagraph = { text = "}", prio = 8 },
+			-- },
+		},
+	},
 }
