e59f90e6cbb29b73da5dabedb7263ab94903a0e6
Sinisa Kusic <contact.sinisa@gmail.com>
2024-10-25 14:31:59 +0200

    feat(lsp): refactor LSP configuration
    - Extracted diagnostic symbols setup to `setup_diagnostic_symbols` function.
    - Extracted diagnostics display configuration to `configure_diagnostics` function.
    - Extracted diagnostic key mappings setup to `setup_diagnostic_keymaps` function.
    - Extracted LSP key mappings setup to `setup_lsp_keymaps` function.
    - Extracted LSP servers configuration to `configure_servers` function.
    - Updated `ts_ls` specific key mappings within `setup_lsp_keymaps`.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,88 @@
+local keymap = vim.keymap -- for conciseness
+local set_desc = require("utils").set_desc
+
+-- Function to set up diagnostic symbols
+local function setup_diagnostic_symbols()
+	for name, icon in pairs(require("config.icons").icons.diagnostics) do
+		name = "DiagnosticSign" .. name
+		vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+	end
+end
+
+-- Function to configure diagnostics display
+local function configure_diagnostics()
+	vim.diagnostic.config({
+		virtual_text = false,
+		float = {
+			source = "always", -- Or "if_many"
+		},
+		table = {
+			source = "always", -- Or "if_many"
+		},
+		signs = true,
+		severity_sort = true,
+	})
+end
+
+-- Function to set up diagnostic key mappings
+local function setup_diagnostic_keymaps(opts)
+	keymap.set("n", "<leader>e", vim.diagnostic.open_float, set_desc(opts, { desc = "Show Line Diagnostics" }))
+	keymap.set("n", "[d", vim.diagnostic.goto_prev, set_desc(opts, { desc = "Go to Previous Diagnostic" }))
+	keymap.set("n", "]d", vim.diagnostic.goto_next, set_desc(opts, { desc = "Go to Next Diagnostic" }))
+	keymap.set("n", "<leader>q", vim.diagnostic.setloclist, set_desc(opts, { desc = "Set Location List" }))
+end
+
+-- Function to set up LSP key mappings
+local function setup_lsp_keymaps(client, bufnr)
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	keymap.set("n", "gD", vim.lsp.buf.declaration, set_desc(bufopts, { desc = "Go to Declaration" }))
+	keymap.set("n", "gd", vim.lsp.buf.definition, set_desc(bufopts, { desc = "Go to Definition" }))
+	keymap.set("n", "K", vim.lsp.buf.hover, set_desc(bufopts, { desc = "Show Hover" }))
+	keymap.set("n", "gi", vim.lsp.buf.implementation, set_desc(bufopts, { desc = "Go to Implementation" }))
+	keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, set_desc(bufopts, { desc = "Show Signature Help" }))
+	keymap.set(
+		"n",
+		"<leader>wa",
+		vim.lsp.buf.add_workspace_folder,
+		set_desc(bufopts, { desc = "Add Workspace Folder" })
+	)
+	keymap.set(
+		"n",
+		"<leader>wr",
+		vim.lsp.buf.remove_workspace_folder,
+		set_desc(bufopts, { desc = "Remove Workspace Folder" })
+	)
+	keymap.set("n", "<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, set_desc(bufopts, { desc = "List Workspace Folders" }))
+	keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, set_desc(bufopts, { desc = "Go to Type Definition" }))
+	keymap.set("n", "<leader>rn", vim.lsp.buf.rename, set_desc(bufopts, { desc = "Rename" }))
+	keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, set_desc(bufopts, { desc = "Code Action" }))
+	keymap.set("n", "gr", vim.lsp.buf.references, set_desc(bufopts, { desc = "Go to References" }))
+
+	if client.name == "ts_ls" then
+		keymap.set(
+			"n",
+			"<leader>co",
+			"<cmd>TypescriptOrganizeImports<CR>",
+			set_desc(bufopts, { desc = "Organize Imports" })
+		)
+		keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", set_desc(bufopts, { desc = "Rename File" }))
+	end
+end
+
+-- Function to configure LSP servers
+local function configure_servers(servers, capabilities, lspconfig)
+	for key, value in pairs(servers) do
+		lspconfig[key].setup(vim.tbl_deep_extend("force", {
+			on_attach = function(client, bufnr)
+				setup_lsp_keymaps(client, bufnr)
+			end,
+			capabilities = capabilities,
+		}, value))
+	end
+end
+
 return {
 	{
 		"williamboman/mason.nvim",
@@ -59,14 +144,9 @@ return {
 		event = "InsertEnter",
 		config = function()
 			local lspconfig = require("lspconfig")
-			local keymap = vim.keymap -- for conciseness
-			local set_desc = require("utils").set_desc
-
-			-- nvim-cmp supports additional completion capabilities
-			-- local capabilities = vim.lsp.protocol.make_client_capabilities()
-			-- capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
 			local servers = {
 				html = {},
 				ts_ls = {},
@@ -87,14 +167,9 @@ return {
 				},
 				lua_ls = {
 					settings = {
-						-- custom settings for lua
 						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
+							diagnostics = { globals = { "vim" } },
 							workspace = {
-								-- make language server aware of runtime files
 								library = {
 									[vim.fn.expand("$VIMRUNTIME/lua")] = true,
 									[vim.fn.stdpath("config") .. "/lua"] = true,
@@ -105,101 +180,10 @@ return {
 				},
 			}
 
-			-- Change diagnostic symbols in the sign column (gutter)
-			for name, icon in pairs(require("config.icons").icons.diagnostics) do
-				name = "DiagnosticSign" .. name
-				vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
-			end
-
-			local opts = { noremap = true, silent = true }
-
-			-- Customizing how diagnostics are displayed
-			vim.diagnostic.config({
-				-- virtual_text = {
-				-- 	source = --- "always", -- Or "if_many"
-				-- 	prefix = "●", -- Could be '■', '▎', 'x'
-				-- },
-				virtual_text = false,
-				float = {
-					source = "always", -- Or "if_many"
-				},
-				table = {
-					source = "always", -- Or "if_many"
-				},
-				signs = true,
-				severity_sort = true,
-			})
-
-			keymap.set("n", "<leader>e", vim.diagnostic.open_float, set_desc(opts, { desc = "Show Line Diagnostics" }))
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, set_desc(opts, { desc = "Go to Previous Diagnostic" }))
-			keymap.set("n", "]d", vim.diagnostic.goto_next, set_desc(opts, { desc = "Go to Next Diagnostic" }))
-			keymap.set("n", "<leader>q", vim.diagnostic.setloclist, set_desc(opts, { desc = "Set Location List" }))
-
-			local on_attach = function(client, bufnr)
-				-- Mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
-				local bufopts = { noremap = true, silent = true, buffer = bufnr }
-				keymap.set("n", "gD", vim.lsp.buf.declaration, set_desc(bufopts, { desc = "Go to Declaration" }))
-				keymap.set("n", "gd", vim.lsp.buf.definition, set_desc(bufopts, { desc = "Go to Definition" }))
-				keymap.set("n", "K", vim.lsp.buf.hover, set_desc(bufopts, { desc = "Show Hover" }))
-				keymap.set("n", "gi", vim.lsp.buf.implementation, set_desc(bufopts, { desc = "Go to Implementation" }))
-				keymap.set(
-					"n",
-					"<C-k>",
-					vim.lsp.buf.signature_help,
-					set_desc(bufopts, { desc = "Show Signature Help" })
-				)
-				keymap.set(
-					"n",
-					"<leader>wa",
-					vim.lsp.buf.add_workspace_folder,
-					set_desc(bufopts, { desc = "Add Workspace Folder" })
-				)
-				keymap.set(
-					"n",
-					"<leader>wr",
-					vim.lsp.buf.remove_workspace_folder,
-					set_desc(bufopts, { desc = "Remove Workspace Folder" })
-				)
-				keymap.set("n", "<leader>wl", function()
-					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, set_desc(bufopts, { desc = "List Workspace Folders" }))
-				keymap.set(
-					"n",
-					"<leader>D",
-					vim.lsp.buf.type_definition,
-					set_desc(bufopts, { desc = "Go to Type Definition" })
-				)
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, set_desc(bufopts, { desc = "Rename" }))
-				keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, set_desc(bufopts, { desc = "Code Action" }))
-				keymap.set("n", "gr", vim.lsp.buf.references, set_desc(bufopts, { desc = "Go to References" }))
-				-- keymap.set("n", "<leader>f", function()
-				-- 	vim.lsp.buf.format({ async = true })
-				-- end, set_desc(bufopts, { desc = "Format Document" }))
-
-				if client.name == "ts_ls" then
-					keymap.set(
-						"n",
-						"<leader>co",
-						"<cmd>TypescriptOrganizeImports<CR>",
-						set_desc(bufopts, { desc = "Organize Imports" })
-					)
-					keymap.set(
-						"n",
-						"<leader>cR",
-						"<cmd>TypescriptRenameFile<CR>",
-						set_desc(bufopts, { desc = "Rename File" })
-					)
-				end
-			end
-
-			-- configure servers
-			for key, value in pairs(servers) do
-				lspconfig[key].setup(vim.tbl_deep_extend("force", {
-					on_attach = on_attach,
-					capabilities = capabilities,
-				}, value))
-			end
+			setup_diagnostic_symbols()
+			configure_diagnostics()
+			setup_diagnostic_keymaps({ noremap = true, silent = true })
+			configure_servers(servers, capabilities, lspconfig)
 		end,
 	},
 }
