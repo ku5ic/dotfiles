9b826e48149bf0526a504bbb1ac8ce2a2203a870
Sinisa Kusic <contact.sinisa@gmail.com>
2024-12-20 10:38:18 +0100

    refactor(install.sh): modularize script functions
    Refactored install.sh to improve readability and maintainability by
    modularizing the script into functions. Added error handling with `set -e`.
    Functions include update_dotfiles, install_homebrew, install_shells,
    source_scripts, create_symlinks, and setup_asdf. Updated symbolic link
    creation and asdf setup for nodejs, ruby, and python.

--- install.sh
+++ install.sh
@@ -1,71 +1,79 @@
-# This script is run in the shell environment
-!/bin/sh
+#!/bin/sh
 
-# The script sets the DOTFILES_DIR environment variable to the .dotfiles directory in the home directory
-export DOTFILES_DIR="$HOME/.dotfiles"
+set -e
 
-# If the .dotfiles directory is a git repository, it pulls the latest changes from the master branch
-[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+DOTFILES_DIR="$HOME/.dotfiles"
 
-# The script installs Homebrew, a package manager for macOS
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+update_dotfiles() {
+  if [ -d "$DOTFILES_DIR/.git" ]; then
+    git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+  fi
+}
 
-# The script installs zsh, a shell, using Homebrew and sets it as the default shell
-brew install zsh
+install_homebrew() {
+  if ! command -v brew >/dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+}
 
-# Add the Homebrew-installed zsh to the list of acceptable shells
-echo $(brew --prefix)/bin/zsh | sudo tee -a /private/etc/shells /usr/local/bin/bash
+install_shells() {
+  brew install zsh bash
 
-# Change the default shell to the Homebrew-installed zsh
-chsh -s /opt/homebrew/bin/zsh
+  echo "$(brew --prefix)/bin/zsh" | sudo tee -a /private/etc/shells
+  echo "$(brew --prefix)/bin/bash" | sudo tee -a /private/etc/shells
 
-# Install the latest version of Bash using Homebrew
-brew install bash
+  chsh -s "$(brew --prefix)/bin/zsh"
+}
 
-# Add the newly installed Bash to the list of valid login shells
-# The command `brew --prefix` returns the Homebrew installation prefix
-# The output is then appended to the /private/etc/shells file and also copied to /usr/local/bin/bash
-echo $(brew --prefix)/bin/bash | sudo tee -a /private/etc/shells /usr/local/bin/bash
+source_scripts() {
+  source "$DOTFILES_DIR/install/brew.sh"
+  source "$DOTFILES_DIR/install/brew-cask.sh"
+  source "$DOTFILES_DIR/install/tmux-plugin-manager.sh"
+}
 
-# The script sources (runs) other scripts to install additional Homebrew and brew-cask packages and tmux plugins
-source "$DOTFILES_DIR/install/brew.sh"
-source "$DOTFILES_DIR/install/brew-cask.sh"
-source "$DOTFILES_DIR/install/tmux-plugin-manager.sh"
+create_symlinks() {
+  ln -sfv "$DOTFILES_DIR/.zshenv" ~
+  ln -sfv "$DOTFILES_DIR/.zshrc" ~
+  ln -sfv "$DOTFILES_DIR/.zprofile" ~
+  ln -sfv "$DOTFILES_DIR/.aliases.zsh" ~
+  ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+  ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
+  ln -sfv "$DOTFILES_DIR/.gitmessage" ~
+  ln -sfv "$DOTFILES_DIR/.gemrc" ~
+  ln -sfv "$DOTFILES_DIR/.tmux.conf" ~
+  ln -sfv "$DOTFILES_DIR/.default-python-packages" ~
+  ln -sfv "$DOTFILES_DIR/tmux/resurrect" ~/.tmux/
 
-# The script creates symbolic links from various configuration files in the .dotfiles directory to the home directory
-ln -sfv "$DOTFILES_DIR/.zshenv" ~
-ln -sfv "$DOTFILES_DIR/.zshrc" ~
-ln -sfv "$DOTFILES_DIR/.zprofile" ~
-ln -sfv "$DOTFILES_DIR/.aliases.zsh" ~
-ln -sfv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
-ln -sfv "$DOTFILES_DIR/.gitmessage" ~
-ln -sfv "$DOTFILES_DIR/.gemrc" ~
-ln -sfv "$DOTFILES_DIR/.tmux.conf" ~
-ln -sfv "$DOTFILES_DIR/.default-python-packages" ~
-ln -sfv "$DOTFILES_DIR/tmux/resurrect" ~/.tmux/
+  mkdir -p ~/.config
+  ln -sfv "$DOTFILES_DIR/config/nvim" ~/.config/
+  ln -sfv "$DOTFILES_DIR/config/wezterm" ~/.config/
+  ln -sfv "$DOTFILES_DIR/config/starship.toml" ~/.config/
+}
 
-# The script creates a .config directory in the home directory and creates symbolic links from various configuration files in the .dotfiles directory to the .config directory
-mkdir ~/.config
-ln -sfv "$DOTFILES_DIR/config/nvim" ~/.config/
-ln -sfv "$DOTFILES_DIR/config/wezterm" ~/.config/
-ln -sfv "$DOTFILES_DIR/config/starship.toml" ~/.config/
+setup_asdf() {
+  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || true
+  asdf nodejs update-nodebuild
+  asdf install nodejs latest:18
+  asdf global nodejs latest:18
 
-# The script sets up Node.js, Ruby, and Python using asdf, a version manager. It adds the necessary plugins, updates them, installs the latest versions of the languages, and sets those versions as the global versions
-# setup node
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf nodejs update-nodebuild
-asdf install nodejs latest:18
-asdf global nodejs latest:18
+  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git || true
+  asdf plugin-update ruby
+  asdf install ruby latest:3
+  asdf global ruby latest:3
 
-# setup ruby
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf plugin-update ruby
-asdf install ruby latest:3
-asdf global ruby latest:3
+  asdf plugin add python || true
+  asdf plugin-update python
+  asdf install python latest:3
+  asdf global python latest:3
+}
 
-# setup python
-asdf plugin add python
-asdf plugin-update python
-asdf install python latest:3
-asdf global python latest:3
+main() {
+  update_dotfiles
+  install_homebrew
+  install_shells
+  source_scripts
+  create_symlinks
+  setup_asdf
+}
+
+main "$@"
