985a74e07baee6f07b5d3ee4c55b7252e9b05a2e
Sinisa Kusic <contact.sinisa@gmail.com>
2025-02-03 10:18:20 +0100

    feat(ui): integrate notify with catppuccin theme
    Added notify integration to the catppuccin theme setup in ui.lua.
    This change ensures that notifications are styled according to the
    catppuccin theme.
    
    Relevant files:
    - config/nvim/lua/plugins/ui.lua

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -4,7 +4,11 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			require("catppuccin").setup()
+			require("catppuccin").setup({
+				integrations = {
+					notify = true,
+				},
+			})
 			vim.cmd([[colorscheme catppuccin-macchiato]])
 		end,
 	},
