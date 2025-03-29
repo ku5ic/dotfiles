72763ce45dac9b4b4f8ccca6a69313b659d0f890
Sinisa Kusic <contact.sinisa@gmail.com>
2025-03-29 16:34:36 +0100

    feat(keymaps): refactor keymap definitions for clarity
    Refactored keymap definitions to improve readability and maintainability.
    Introduced a utility function `map` for concise keymap setting with
    descriptions. Grouped related keymaps into tables and iterated over them
    to reduce redundancy.
    
    This change simplifies the keymap configuration and makes it easier to
    add or modify mappings in the future.

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,37 +1,43 @@
-
--- set leader key to space
+-- Set leader key to space
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 local opts = { noremap = true, silent = true } -- Silent keymap option
 
-local set_desc = require("utils").set_desc
----------------------
+-- Utility function for setting keymaps with descriptions
+local function map(mode, lhs, rhs, desc)
+	keymap.set(mode, lhs, rhs, require("utils").set_desc(opts, { desc = desc }))
+end
+
 -- General Keymaps
----------------------
+map("n", "<leader>nh", ":nohl <CR>", "Clear search highlights")
 
--- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl <CR>", set_desc(opts, { desc = "Clear search highlights" }))
+-- Window management
+local window_mappings = {
+	{ "<leader>sv", "<C-w>v", "Split Window Vertically" },
+	{ "<leader>sh", "<C-w>s", "Split Window Horizontally" },
+	{ "<leader>se", "<C-w>=", "Make Split Window Equal Width & Height" },
+	{ "<leader>sx", ":close <CR>", "Close Current Window" },
+	{ "<leader>to", ":tabnew <CR>", "Open New Tab" },
+	{ "<leader>tx", ":tabclose <CR>", "Close Current Tab" },
+	{ "<leader>tn", ":tabn <CR>", "Go To Next Tab" },
+	{ "<leader>tp", ":tabp <CR>", "Go To Previous Tab" },
+}
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", set_desc(opts, { desc = "Split Window Vertically" }))
-keymap.set("n", "<leader>sh", "<C-w>s", set_desc(opts, { desc = "Split Window Horizontally" }))
-keymap.set("n", "<leader>se", "<C-w>=", set_desc(opts, { desc = "Make Split Window Equal Width & Height" }))
-keymap.set("n", "<leader>sx", ":close <CR>", set_desc(opts, { desc = "Close Current Window" }))
+for _, mapping in ipairs(window_mappings) do
+	map("n", mapping[1], mapping[2], mapping[3])
+end
 
-keymap.set("n", "<leader>to", ":tabnew <CR>", set_desc(opts, { desc = "Open New Tab" }))
-keymap.set("n", "<leader>tx", ":tabclose <CR>", set_desc(opts, { desc = "Close Current Tab" }))
-keymap.set("n", "<leader>tn", ":tabn <CR>", set_desc(opts, { desc = "Go To Next Tab" }))
-keymap.set("n", "<leader>tp", ":tabp <CR>", set_desc(opts, { desc = "Go To Previous Tab" }))
+-- Move lines
+local move_mappings = {
+	{ "n", "<A-j>", ":m .+1<CR>==", "Move Line Down" },
+	{ "n", "<A-k>", ":m .-2<CR>==", "Move Line Up" },
+	{ "i", "<A-j>", "<Esc>:m .+1<CR>==gi", "Move Line Down" },
+	{ "i", "<A-k>", "<Esc>:m .-2<CR>==gi", "Move Line Up" },
+	{ "v", "<A-j>", ":m '>+1<CR>gv=gv", "Move Selection Down" },
+	{ "v", "<A-k>", ":m '<-2<CR>gv=gv", "Move Selection Up" },
+}
 
--- move lines
-local move_opts = { noremap = true, silent = true }
-
-keymap.set("n", "<A-j>", ":m .+1<CR>==", set_desc(move_opts, { desc = "Move Line Down" }))
-keymap.set("n", "<A-k>", ":m .-2<CR>==", set_desc(move_opts, { desc = "Move Line Up" }))
-
-keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", set_desc(move_opts, { desc = "Move Line Down" }))
-keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", set_desc(move_opts, { desc = "Move Line Up" }))
-
-keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", set_desc(move_opts, { desc = "Move Selection Down" }))
-keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", set_desc(move_opts, { desc = "Move Selection Up" }))
+for _, mapping in ipairs(move_mappings) do
+	map(mapping[1], mapping[2], mapping[3], mapping[4])
+end
