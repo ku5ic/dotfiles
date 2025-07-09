face1f42aa3da9a17c21ba3128ff1cf86befe3af
Sinisa Kusic <contact.sinisa@gmail.com>
2025-07-09 11:12:07 +0200

    refactor(cmp): simplify mappings and sources config
    Simplified nvim-cmp configuration by removing custom Tab/Shift-Tab mapping
    functions and unused sources. Updated mappings to use built-in select item
    functions and removed LuaSnip and spell sources for clarity. Also added an
    entry filter to exclude snippet completions. This streamlines the code
    completion setup and improves maintainability.

--- config/nvim/lua/plugins/code-completion.lua
+++ config/nvim/lua/plugins/code-completion.lua
@@ -14,53 +14,28 @@ return {
 		config = function()
 			local cmp = require("cmp")
 
-			-- Helper function to check if there are words before the cursor
-			local function has_words_before()
-				local _, col = unpack(vim.api.nvim_win_get_cursor(0))
-				if col == 0 then
-					return false
-				end
-				return not vim.api.nvim_get_current_line():sub(col, col):match("%s")
-			end
-
-			-- Define mappings for completion
-			local function tab_mapping(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif has_words_before() then
-					cmp.complete()
-				else
-					fallback()
-				end
-			end
-
-			local function shift_tab_mapping(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				else
-					fallback()
-				end
-			end
-
 			-- Configure nvim-cmp
+			--
 			cmp.setup({
 				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = cmp.mapping(tab_mapping, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(shift_tab_mapping, { "i", "s" }),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
 				}),
+
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp", group_index = 1 },
-					{ name = "luasnip", group_index = 1 },
-					{ name = "buffer", group_index = 2 },
-					{ name = "path", group_index = 2 },
-					{ name = "spell", group_index = 3 },
-					{ name = "copilot", group_index = 3 },
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "copilot" },
 				}),
+
+				entry_filter = function(entry, ctx)
+					local item = entry:get_completion_item()
+					return item.insertTextFormat ~= 2
+				end,
+
 				formatting = {
 					format = function(entry, item)
 						local icons = require("config.icons").icons.kinds
@@ -71,7 +46,6 @@ return {
 							copilot = "[Copilot]",
 							path = "[Path]",
 							nvim_lsp = "[LSP]",
-							luasnip = "[LuaSnip]",
 							spell = "[Spell]",
 							buffer = "[Buffer]",
 						})[entry.source.name]
