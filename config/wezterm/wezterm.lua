7693be106a948106f0c54e32402124dab85c289f
Sinisa Kusic <contact.sinisa@gmail.com>
2025-07-10 13:00:04 +0200

    refactor(wezterm): simplify config to use table return
    Refactored wezterm configuration to directly return a table instead of
    using an intermediate config variable and config_builder. This reduces
    boilerplate and improves readability. Keybindings and all settings are
    now defined inline in the returned table.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,52 +1,39 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
-local config = {}
+return {
+	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font("FiraCode Nerd Font Propo", {
+		weight = 450,
+		stretch = "Normal",
+		style = "Normal",
+	}), -- (AKA: FiraCode Nerd Font Propo Ret) /Users/ku5ic/Library/Fonts/FiraCodeNerdFontPropo-Retina.ttf, CoreText
+	freetype_load_target = "Light",
+	freetype_render_target = "HorizontalLcd",
+	font_size = 14,
+	front_end = "WebGpu",
+	initial_rows = 33,
+	initial_cols = 100,
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 0,
+		bottom = 0,
+	},
+	native_macos_fullscreen_mode = true,
+	window_background_opacity = 0.95,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	enable_scroll_bar = false,
+	enable_csi_u_key_encoding = true,
+	audible_bell = "Disabled",
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
-config.color_scheme_dirs = { "~/.local/share/nvim/lazy/tokyonight.nvim/extras/wezterm/" }
--- config.color_scheme = "Tokyo Night Storm"
-config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("FiraCode Nerd Font Propo", {
-	weight = 450,
-	stretch = "Normal",
-	style = "Normal",
-}) -- (AKA: FiraCode Nerd Font Propo Ret) /Users/ku5ic/Library/Fonts/FiraCodeNerdFontPropo-Retina.ttf, CoreText
-config.freetype_load_target = "Light"
-config.freetype_render_target = "HorizontalLcd"
-config.font_size = 14
-config.front_end = "WebGpu"
-config.initial_rows = 33
-config.initial_cols = 100
-config.window_padding = {
-	left = 20,
-	right = 20,
-	top = 0,
-	bottom = 0,
-}
-config.native_macos_fullscreen_mode = true
-config.window_background_opacity = 0.95
-config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-config.enable_scroll_bar = false
-config.enable_csi_u_key_encoding = true
-config.audible_bell = "Disabled"
-
--- Keybindings
-config.keys = {
-	{
-		key = "v",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.DisableDefaultAssignment,
+	-- Keybindings
+	keys = {
+		{
+			key = "v",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.DisableDefaultAssignment,
+		},
 	},
 }
-
--- and finally, return the configuration to wezterm
-return config
