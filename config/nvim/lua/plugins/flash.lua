6b84900d8f4b5741df85c57da36b5cd334276a38
Sinisa Kusic <contact.sinisa@gmail.com>
2026-06-02 14:12:05 +0200

    fix(nvim): remove s/S/r/R overrides from flash.nvim
    flash still enhances / search with labels and adds labels to
    f/F/t/T when multiple targets exist. No default motions overridden.

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -10,40 +10,5 @@ return {
         },
       },
     },
-    keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-        desc = "Flash jump",
-      },
-      {
-        "S",
-        -- Deliberately excludes "x" (visual) to preserve nvim-surround's visual S binding
-        mode = { "n", "o" },
-        function()
-          require("flash").treesitter()
-        end,
-        desc = "Flash treesitter select",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote()
-        end,
-        desc = "Flash remote",
-      },
-      {
-        "R",
-        mode = { "o", "x" },
-        function()
-          require("flash").treesitter_search()
-        end,
-        desc = "Flash treesitter search",
-      },
-    },
   },
 }
