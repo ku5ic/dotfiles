19bc17e2af64c2ff4ce44ffc9de4321e1ed2e44f
Sinisa Kusic <contact.sinisa@gmail.com>
2026-06-02 18:51:01 +0200

    refactor(nvim): split mason-nvim-dap plugin spec into its own file
    Move the mason.nvim DAP integration out of the debuggers plugin spec and into
    a dedicated mason-nvim-dap.lua plugin file. This separates concerns and keeps
    plugin specs modular while preserving existing behavior.

---
+++ config/nvim/lua/plugins/mason-nvim-dap.lua
@@ -0,0 +1,17 @@
+return {
+  -- mason.nvim integration
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = "mason.nvim",
+    cmd = { "DapInstall", "DapUninstall" },
+    opts = {
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
+        "php",
+        "js",
+      },
+    },
+  },
+}
