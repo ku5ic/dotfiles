66bf719b14b5bd00cd4f042f924838ea8ddc17e9
Sinisa Kusic <contact.sinisa@gmail.com>
2026-06-02 14:41:27 +0200

    fix(nvim): replace fs/fS flash bindings with leader>j/J
    fs/fS conflicted with the built-in f{char} motion - Neovim would
    wait timeoutlen before deciding which to use every time f+s was typed.
    
    <leader>j -> flash jump (n/x/o)
    <leader>J -> flash treesitter select (n/x/o)

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -4,10 +4,31 @@ return {
     event = "VeryLazy",
     opts = {
       modes = {
+        search = {
+          enabled = true,
+        },
         char = {
           -- Show jump labels on f/F/t/T so multi-occurrence jumps don't require ; repeats
           jump_labels = true,
         },
+      },
+    },
+    keys = {
+      {
+        "<leader>j",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash jump",
+      },
+      {
+        "<leader>J",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash treesitter",
       },
     },
   },
