5f4936f62f020b4c30c58caa86264a5e72506e82
Sinisa Kusic <contact.sinisa@gmail.com>
2026-06-04 12:58:19 +0200

    feat(nvim): add nvim-dap-ui debug UI and keymap
    - Add plugin spec to enable rcarriga/nvim-dap-ui with dependencies on
      mfussenegger/nvim-dap and nvim-neotest/nvim-nio. Register <leader>du to
      toggle the debug UI.
    - Configure dapui to open when a debug session starts and close on exit.
      Update lazy lockfile to include the new plugins and remove the unused
      entry.

---
+++ config/nvim/lua/plugins/nvim-dap-ui.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    keys = {
+      {
+        "<leader>du",
+        function()
+          require("dapui").toggle()
+        end,
+        desc = "Toggle debug UI",
+      },
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+
+      dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+      dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+      dap.listeners.before.event_exited["dapui_config"] = dapui.close
+    end,
+  },
+}
